5635602a550195ac957da571ad4160b8e34292ce
DavidChristopherNelson <david.nelson1@uqconnect.edu.au>
2026-03-31 14:28:34 -0700

    Rename migration.

---
+++ src/interfaces/database/migrations/1774992421947-InitialSchema.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialSchema1774992421947 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
